d608d03426d9bf501bcf14e73473709e1bbb501c
Derya Gülmez <deryaglmz0@gmail.com>
2023-07-20 22:02:33 +0300

    project2

---
+++ project2.py
@@ -0,0 +1,18 @@
+def main():
+    print("Enter the numbers you want to sum. Press 'q' to exit.")
+
+    total = 0
+    while True:
+        try:
+            user_input = input("Enter a number: ")
+            if user_input.lower() == 'q':
+                break
+            number = float(user_input)
+            total += number
+        except ValueError:
+            print("Invalid input. Please enter a number or press 'q' to exit.")
+
+    print("Sum of the entered numbers:", total)
+
+if __name__ == "__main__":
+    main()
