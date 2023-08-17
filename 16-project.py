02420a5d6f18dd04521f9181039940e9b145510d
Derya Gülmez <deryaglmz0@gmail.com>
2023-08-17 15:55:31 +0300

    Project

---
+++ 16-project.py
@@ -0,0 +1,36 @@
+
+# username, password => database
+# 'deryagulmez'  , '123456'
+
+# == : is it equal?
+# != : isn't it equal?
+# > : is it big?
+# < : is it small?
+# >= : Is it equal to big?
+# <= : Is it equal to small?
+
+a, b, c, d = 5, 5, 10, 4
+
+password = '1234'
+username = 'deryagulmez'
+
+result = a == b   # True
+result = a == c   # False
+result = ('dryglmz' == username)
+result = ('deryagulmez' == username)
+result = a != b   # False
+result = a != c
+result = a > b
+result = a > c
+result = a < b
+result = a < c
+result = a <= b
+result = a <= c
+result = a >= b
+result = a >= c
+result = (True == 1)
+result = (False == 0)
+result = (False + True + 40)
+
+
+print(result)
